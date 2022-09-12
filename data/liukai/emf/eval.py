fefe076f8f0545b90ed4f233c86e1ab5778ce0cf
zerkvii <zerkvii@gmail.com>
2022-09-12 23:01:21 +0800

    mark master

--- data/liukai/emf/eval.py
+++
@@ -1,7 +0,0 @@
-import torch
-import torch.nn as nn
-from torchmetrics.text import cer, wer
-from torch.utils.data import DataLoader
-
-if __name__ == '__main__':
-    print('evaluate')
--- data/liukai/emf/opt_test.py
+++
@@ -1,9 +0,0 @@
-from transformers import GPT2Tokenizer
-from opt.opt_model import OPTModel,OPTForCausalLM
-import torch
-
-tokenizer = GPT2Tokenizer.from_pretrained("facebook/opt-350m")
-origin_model = OPTModel.from_pretrained("facebook/opt-350m")
-gen_model=OPTForCausalLM.from_pretrained('facebook/opt-350m')
-inputs = tokenizer("Hello,my dog is cute", return_tensors="pt")
-print(gen_model.generate(inputs))
