b54e4132c0b1229a4c8f3793fbbf768b9c6f01de
zerkvii <zerkvii@gmail.com>
2022-09-12 22:50:49 +0800

    add seed

---
+++ evaluate.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+if __name__ == '__main__':
+    print('eval')
